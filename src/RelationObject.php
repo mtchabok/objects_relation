f9d4a7805c8099515c551fab730ecefa982a2ac5
Mohammad Taghi Chabok <mtchabok@users.noreply.github.com>
2019-06-07 15:47:43 +0430

    Create RelationObject.php

---
+++ src/RelationObject.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Project: mtchabok_objects_relation
+ * User: mtchabok
+ * Date: 2019-06-07
+ * Time: 3:52 AM
+ */
+
+namespace Mtchabok\ObjectsRelation;
+
+/**
+ * Class RelationObject
+ * @package Mtchabok\ObjectsRelation
+ */
+class RelationObject
+{
+	use Relation;
+}
